237b243dbf3c6260dc0fa109691a563d509b4d33
Ashutosh Mohanty <ashutoshmohanty3815@gmail.com>
2022-08-21 00:39:24 +0530

    Hello Viewers

--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'tp4ln3j8',
+  projectId: 'iv325jhf',
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
